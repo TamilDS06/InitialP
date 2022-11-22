cf4b2ed3cf34666933ec04a3053302ee66c5ba4f
Tamil06 <mtsmech04@gmail.com>
2022-11-22 12:53:27 +0530

    prime_list

--- BasicLogics.py
+++ BasicLogics.py
@@ -6,3 +6,13 @@ def is_prime(num):
             else:
                 return False
     return True
+
+# c = is_prime(44)
+# print(c)
+
+def cal_prime(start,end):
+    prime_list = []
+    for i in range(start,end):
+        if is_prime(i):
+            prime_list.append(i)
+    return prime_list

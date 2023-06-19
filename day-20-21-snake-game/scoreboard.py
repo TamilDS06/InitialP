fa2a7e91e4257feec1b3643d260a57cb7ff40db6
Tamilselvan Mani <mtsmech04@gmail.com>
2023-06-19 08:19:17 +0530

    High score update file  added in the snake-game

--- day-20-21-snake-game/scoreboard.py
+++ day-20-21-snake-game/scoreboard.py
@@ -8,8 +8,10 @@ class ScoreBoard(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.high_score = None
         self.Score = 0
-        self.high_score = 0
+        with open('C:\\My_Folder\\Logics_in_python\\day-20-21-snake-game\\data.txt', mode='r') as data:
+            self.high_score = int(data.read())
         self.color('white')
         self.penup()
         self.goto(0, 270)
@@ -23,9 +25,11 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.Score > self.high_score:
             self.high_score = self.Score
+            with open('C:\\My_Folder\\Logics_in_python\\day-20-21-snake-game\\data.txt', mode='w') as data:
+                data.write(f"{self.high_score}")
         self.Score = 0
         self.update_scoreboard()
-        
+
     # def game_over(self):
     #     self.goto(0, 0)
     #     self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)

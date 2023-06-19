6e2f0815ea2c27d933d6bf2cf082f895ab76da00
Tamilselvan Mani <mtsmech04@gmail.com>
2023-06-19 16:30:17 +0530

    Mail Merge added

---
+++ Mail Merge Project Start/main.py
@@ -0,0 +1,12 @@
+PLACE_HOLDER = "[name]"
+
+with open('./Mail Merge Project Start/Input/Names/invited_names.txt', 'r') as list_names:
+    names = list_names.readlines()
+
+with open('./Mail Merge Project Start/Input/Letters/starting_letter.txt', 'r') as example_letter:
+    new_letter = example_letter.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = new_letter.replace(PLACE_HOLDER, stripped_name)
+        with open(f'./Mail Merge Project Start/Output/ReadyToSend/Letter_to_{name.strip()}', 'w') as file_write:
+            file_write.write(new_letter)
